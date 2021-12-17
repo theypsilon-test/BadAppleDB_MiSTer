01cafe6372249202edeb0c8de9a25578fa58f0fe
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-12-17 21:06:24 +0100

    New last screen.

--- build_db.py
+++ build_db.py
@@ -24,10 +24,10 @@ from zipfile import ZipFile
 def main():
     print('START!')
 
-    make_db('bad_apple_full_res_db.json', 'data_full_res.txt', 0.07217318)
-    make_db('bad_apple_half_res_db.json', 'data_half_res.txt', 0.0925)
+    make_db('bad_apple_full_res_db.json', 'data_full_res.txt', 0.07217318, 22*2)
+    make_db('bad_apple_half_res_db.json', 'data_half_res.txt', 0.0925, 22*1)
 
-def make_db(file_name, input, wait_time):
+def make_db(file_name, input, wait_time, height):
     db = {
         "db_id": 'bad_apple_db',
         "db_files": [],
@@ -36,13 +36,13 @@ def make_db(file_name, input, wait_time):
         "zips": {},
         "base_files_url": "",
         "default_options": {},
-        "header": bad_apple_header(input, wait_time),
+        "header": bad_apple_header(input, wait_time, height),
         "timestamp":  int(time.time())
     }
 
     save_json(db, file_name)
 
-def bad_apple_header(input, wait_time):
+def bad_apple_header(input, wait_time, height):
     with open(input, 'rt') as fin:
         data = fin.read()
 
@@ -52,7 +52,28 @@ def bad_apple_header(input, wait_time):
         header.append(page + '\r')
         header.append(wait_time)
     
-    header.append(10.0)
+    header.append(4.0)
+    header.append('\033[H\033[2J')
+
+    page = ''
+    for _ in range(int(height/2 - 4)):
+        page += '\n'
+
+    if height == 22:
+        #       '                                                                                \n'
+        page += '                                 by theypsilon                                  \n'
+        page += '                                                                                \n'
+        page += '                  based on github.com/Chion82/ASCII_bad_apple                   \n'
+
+    else:
+        #       '                                                                                                                                                                \n'
+        page += '                                                                         by theypsilon                                                                          \n'
+        page += '                                                                                                                                                                \n'
+        page += '                                                          based on github.com/Chion82/ASCII_bad_apple                                                           \n'
+
+    header.append(page)
+    header.append(8.0)
+
     return header
 
 def save_json(db, json_name):
